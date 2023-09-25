a03307ee869141504ae9ebc6cdf4e645d4a52f9d
Josh Pearlman <82927774+joshpman@users.noreply.github.com>
2023-09-25 09:48:42 -0400

    Push

---
+++ scripts/nav.js
@@ -0,0 +1,11 @@
+let startScrollPos = window.scrollY;
+
+window.onscroll=function(){
+    let currentScrollPos = window.scrollY;
+    if(startScrollPos>currentScrollPos){
+        document.getElementById("navHeader").style.top="0px";
+    }else{
+        document.getElementById("navHeader").style.top="-100px";
+        startScrollPos=currentScrollPos;
+    }
+}
