db9b70f0bc5765fe2f4c3667f66d014711233bef
Josh Pearlman <82927774+joshpman@users.noreply.github.com>
2023-10-08 16:17:12 -0400

    Mobile Nav Almost done

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,12 +1,53 @@
 let startScrollPos = window.scrollY;
-
+const hamMenu = document.getElementById("mobileMenu");
+const closeMenu = document.getElementById("mobileMenuClose");
+const header = document.getElementsByClassName("header")[0];
+const headerWrapper = document.getElementsByClassName("header__wrapper")[0];
+const projectButton = document.getElementById("projects__wrapper");
+const resumeButton = document.getElementById("resume__wrapper");
+const logoFooter = document.getElementsByClassName("logoFooter")[0];
+const githubLogoFooter = document.getElementById("github__footer");
+const linkedInFooter = document.getElementById("linkedin__footer");
+const body = document.body;
+let inMobile = false;
 window.onscroll=function(){
     let currentScrollPos = window.scrollY;
-    if(startScrollPos>currentScrollPos){
-        document.getElementById("navHeader").style.top="0px";
-    }else{
-        document.getElementById("navHeader").style.top="-175px";
+    if(inMobile===false){
+        if(startScrollPos>currentScrollPos){
+            document.getElementById("navHeader").style.top="0px";
+        }else{
+            document.getElementById("navHeader").style.top="-175px";
+        }
     }
    
     startScrollPos=currentScrollPos;
 }
+
+hamMenu.addEventListener('click', ()=>{
+   header.style.height = "100%";
+   headerWrapper.style.height= "100vh";
+   projectButton.style.display = "block";
+   resumeButton.style.display = "block";
+   hamMenu.style.display = "none";
+   closeMenu.style.display = "block";
+    body.style.overflow="hidden";
+    logoFooter.classList.add("logoNav");
+    githubLogoFooter.classList.add("githubNav");
+    linkedInFooter.classList.add("linkedinNav");
+   inMobile = true;
+   
+
+});
+closeMenu.addEventListener('click', ()=>{
+    header.style.height = "10%";
+    headerWrapper.style.height= "10vh";
+    projectButton.style.display = "none";
+   resumeButton.style.display = "none";
+    closeMenu.style.display = "none";
+    hamMenu.style.display = "block";
+    body.style.overflow="visible";
+    logoFooter.classList.remove("logoNav");
+    githubLogoFooter.classList.remove("githubNav");
+    linkedInFooter.classList.remove("linkedinNav");
+    inMobile = false;
+ });
