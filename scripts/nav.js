2d4dc8ebd7879ee665d966f4c3b5b1f4a3cf26bf
Josh Pearlman <82927774+joshpman@users.noreply.github.com>
2023-10-08 22:10:43 -0400

    Added mobile home button

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,7 +2,9 @@ let startScrollPos = window.scrollY;
 const hamMenu = document.getElementById("mobileMenu");
 const closeMenu = document.getElementById("mobileMenuClose");
 const header = document.getElementsByClassName("header")[0];
+const homeWrapper = document.getElementById("home__wrapper");
 const headerWrapper = document.getElementsByClassName("header__wrapper")[0];
+const headerNav = document.getElementById("navHeader");
 const projectButton = document.getElementById("projects__wrapper");
 const resumeButton = document.getElementById("resume__wrapper");
 const logoFooter = document.getElementsByClassName("logoFooter")[0];
@@ -28,12 +30,14 @@ hamMenu.addEventListener('click', ()=>{
    headerWrapper.style.height= "100vh";
    projectButton.style.display = "block";
    resumeButton.style.display = "block";
+   homeWrapper.style.display = "block";
    hamMenu.style.display = "none";
    closeMenu.style.display = "block";
     body.style.overflow="hidden";
     logoFooter.classList.add("logoNav");
     githubLogoFooter.classList.add("githubNav");
     linkedInFooter.classList.add("linkedinNav");
+    headerNav.classList.add("headerNav");
    inMobile = true;
    
 
@@ -43,11 +47,13 @@ closeMenu.addEventListener('click', ()=>{
     headerWrapper.style.height= "10vh";
     projectButton.style.display = "none";
    resumeButton.style.display = "none";
+   homeWrapper.style.display = "none";
     closeMenu.style.display = "none";
     hamMenu.style.display = "block";
     body.style.overflow="visible";
     logoFooter.classList.remove("logoNav");
     githubLogoFooter.classList.remove("githubNav");
     linkedInFooter.classList.remove("linkedinNav");
+    headerNav.classList.remove("headerNav");
     inMobile = false;
  });
