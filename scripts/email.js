0b543c8d8835fb462a79e5d652183ceeb7a9893b
Josh Pearlman <82927774+joshpman@users.noreply.github.com>
2023-10-08 21:57:39 -0400

    Fixed sizing issue and added email script in

---
+++ scripts/email.js
@@ -0,0 +1,4 @@
+const contactButton = document.getElementsByClassName("contact__button")[0];
+contactButton.addEventListener('click', ()=>{
+    window.open("mailto:joshpearlman2003@gmail.com?subject=Reaching Out&body=Hey Josh, How's it going? ");
+});
